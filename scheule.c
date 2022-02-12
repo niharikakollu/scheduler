9e9066c4c104a3293357c18a32c6c0f50fced5c5
niharikakollu <79960484+niharikakollu@users.noreply.github.com>
2022-02-12 09:43:57 +0530

    Create scheule.c

---
+++ scheule.c
@@ -0,0 +1,146 @@
+#include "esp_log.h"
+#include "lauxlib.h"
+#include "lnodeaux.h"
+#include "module.h"
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+#define ADD_TABLE_ITEM(L, key, val) \
+  lua_pushstring (L, val);      \
+  lua_setfield (L, -2, key);
+ struct task_table{
+  const char *name;
+  const char *task;
+  const char *status;
+};
+struct localstruct{
+ int *zone_id,*duration_mins,*seq_num;
+};
+static const char* ARITHMETIC_METATABLE = NODEMCU_MODULE_METATABLE();
+void inline structure_ToTable(lua_State *L, struct task_table *test )
+{ 
+  lua_createtable (L, 0, 3);
+  ADD_TABLE_ITEM (L, "name", test->name );
+  ADD_TABLE_ITEM (L, "task", test->task);
+  ADD_TABLE_ITEM (L, "status", test->status);
+}
+static int teal_scheduler_table(lua_State *L){
+ int i=0;
+size_t datalen;
+struct localstruct SPprog_tab;
+ if( lua_istable( L,1)){
+ lua_getfield(L,1,"zone_id");
+ if (lua_type(L, -1)==LUA_TTABLE){
+    printf("failed");
+    datalen = lua_objlen(L,1);
+    SPprog_tab.zone_id=(int*)calloc(datalen, sizeof(int));
+	for( i = 0; i < datalen; i ++ ){
+		lua_rawgeti( L, 1, i + 1 );
+     SPprog_tab.zone_id[i]=lua_tointeger (L, -1);
+     printf("%d\t",SPprog_tab.zone_id[i]);
+      lua_pop (L, 2);
+    }
+ }
+ else
+	 luaL_error (L, "Not an array");
+ lua_getfield(L,1,"duration_mins");
+ if (lua_type(L, -1) == LUA_TTABLE){
+     datalen = lua_objlen(L,1);
+    printf("failed1\n");
+    SPprog_tab.duration_mins=(int*)calloc(datalen, sizeof(int));
+    for( i = 0; i < datalen; i ++ ){
+		lua_rawgeti( L, 1, i + 1 );
+     SPprog_tab.duration_mins[i]=lua_tointeger (L, -1);
+     printf("%d\t",SPprog_tab.duration_mins[i]);
+      lua_pop (L, 2);
+    }
+ }
+ else
+   luaL_error (L, "Not an array");
+ lua_getfield(L,1,"seq_num");
+ if (lua_type(L, -1) == LUA_TTABLE){
+   datalen = lua_objlen( L,1 );
+   printf("failed2\n");
+   SPprog_tab.seq_num=(int*)calloc(datalen, sizeof(int));
+   int i=0;
+  for( i = 0; i < datalen; i ++ ){
+		lua_rawgeti( L, 1, i + 1 );
+     SPprog_tab.seq_num[i]=lua_tointeger (L, -1);
+     printf("%d\t",SPprog_tab.seq_num[i]);
+      lua_pop (L, 2);
+  }
+ }
+ else
+   luaL_error (L, "Not an array"); 
+ }
+ return 0;
+}
+
+
+// decimal to binary module
+static int decimal_binary(lua_State* L){  
+  int dec_num=luaL_checknumber(L,1);
+  long long bin_num=0;
+  int rem, i = 1;
+    while (dec_num!=0) {
+    rem = dec_num % 2;
+    dec_num /= 2;
+    bin_num+= rem * i;
+    i *= 10;
+  }
+  lua_pushnumber(L, (lua_Number)bin_num);
+    return 1;
+}
+//  lopading table with key values.
+static int table_key(lua_State* L){  
+  struct task_table test;
+  luaL_checkanytable (L, 1);
+  lua_getfield (L, 1, "name");
+  test.name = luaL_optstring(L, -1, "nill");
+  lua_getfield (L, 1, "task");
+  test.task =  luaL_optstring(L, -1, "nill");
+  lua_getfield (L, 1, "status");
+  test.status =  luaL_optstring(L, -1, "nill");
+  printf("table elemnts are:%s\t%s\t%s\n",test.name,test.task,test.status);
+  structure_ToTable(L,&test);
+    return 1;
+}
+ static int load_arr(lua_State* L){
+  size_t datalen, i;
+   lua_newtable( L ); 
+     if( lua_istable( L,1)){
+     datalen = lua_objlen( L,1 );
+      for( i = 0; i < datalen; i ++ ){
+      lua_rawgeti( L, 1, i + 1 );
+    if(lua_type(L,-1)== LUA_TNUMBER){ 
+    lua_pushnumber(L, (lua_Number)luaL_checknumber(L,-1));
+     lua_pop( L, 1 );
+     lua_rawseti( L,-2, i + 1 );
+    }
+    else if(lua_type(L,-1)== LUA_TSTRING){
+     lua_pushstring(L,luaL_checkstring(L,-1));
+     lua_pop( L, 1 );
+     lua_rawseti( L,-2, i + 1 );
+    }
+      }
+     } 
+   return 1;
+ }
+LROT_BEGIN(arith_metatable)
+LROT_END(arith_metatable, NULL, 0)
+
+LROT_BEGIN(module)
+LROT_FUNCENTRY(larr,load_arr)
+LROT_FUNCENTRY(tkey,table_key)
+LROT_FUNCENTRY(sconcat,string_concat)
+LROT_FUNCENTRY(dectobin,decimal_binary)
+  LROT_FUNCENTRY(schedule_table,teal_scheduler_table)
+LROT_END(module, NULL, 0)
+
+// module_init is invoked on device startup
+static int module_init(lua_State* L) {
+    luaL_rometatable(L, ARITHMETIC_METATABLE, (void*)arith_metatable_map);  // create metatable for arithmetic
+    return 0;
+}
+
+NODEMCU_MODULE_STD();  // define Lua entries
