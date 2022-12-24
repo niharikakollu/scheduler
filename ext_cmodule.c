43462334e6e87a3c59cb757a95a08cf888959f76
niharikakollu <79960484+niharikakollu@users.noreply.github.com>
2022-12-24 14:28:14 +0530

    Update ext_cmodule.c

--- ext_cmodule.c
+++ ext_cmodule.c
@@ -40,12 +40,13 @@ static void lora_sending(lua_State *L){
 }
 
 static void lora_receiving(lua_State *L){
-	int size=0,x=0;
+	int size=0,x=0, rec_flg=0;
 	size=luaL_checknumber(L,1);
 	char buf[size];
 	printf("check8");
 	lora_receive();    // put into receive mode
-	while(lora_available()) {
+	rec_flg=lora_available();
+	while(rec_flg) {
 	 printf("check9");
          x= lora_receive_packet((uint8_t*) buf, sizeof(buf));
          printf("Received: %s, length: %d\n", buf,x);
